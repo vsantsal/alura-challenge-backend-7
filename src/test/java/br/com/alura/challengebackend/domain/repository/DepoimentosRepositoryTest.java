299f3b8b9458997868cd1ab62acbdc146eb4bc8d
vsantsal <vsantsal@gmail.com>
2023-07-19 20:22:57 -0300

    feat: restricoes adicionais sobre entity Depoimento

--- src/test/java/br/com/alura/challengebackend/domain/repository/DepoimentosRepositoryTest.java
+++ src/test/java/br/com/alura/challengebackend/domain/repository/DepoimentosRepositoryTest.java
@@ -2,10 +2,12 @@ package br.com.alura.challengebackend.domain.repository;
 
 import br.com.alura.challengebackend.domain.entity.Depoimento;
 import jakarta.persistence.EntityManager;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
@@ -14,6 +16,7 @@ import java.util.List;
 
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.contains;
 
@@ -81,6 +84,102 @@ class DepoimentosRepositoryTest {
         // Assert
         assertThat(depoimentos.size(), equalTo(3));
 
+    }
+
+    @DisplayName("Não é possível salvar depoimento sem depoente")
+    @Test
+    public void testCenario4() {
+        // Arrange / Act / Assert
+        assertThatThrownBy(
+                () -> repository.save(
+                        new Depoimento(
+                        null,
+                        "Meu Depoimento 1", "Minha Imagem")))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @DisplayName("Não é possível salvar depoimento sem depoimento")
+    @Test
+    public void testCenario5() {
+        // Arrange / Act / Assert
+        assertThatThrownBy(
+                () -> repository.save(
+                        new Depoimento(
+                                "Meu nome",
+                                null, "Minha Imagem")))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @DisplayName("Não é possível salvar depoimento com depoente com mais de 120 caracteres")
+    @Test
+    public void testCenario6() {
+        // Arrange / Act / Assert
+        assertThatThrownBy(
+                () -> repository.save(
+                        new Depoimento(
+                                "a".repeat(121),
+                                "b".repeat(500), "Minha Imagem")))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @DisplayName("Não é possível salvar depoimento com depoimento com mais de 500 caracteres")
+    @Test
+    public void testCenario7() {
+        // Arrange / Act / Assert
+        assertThatThrownBy(
+                () -> repository.save(
+                        new Depoimento(
+                                "a".repeat(120),
+                                "b".repeat(501), "Minha Imagem")))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @DisplayName("Não é possível salvar depoimento com nome de depoente vazio")
+    @Test
+    public void testCenario8() {
+        // Arrange / Act / Assert
+        assertThatThrownBy(
+                () -> repository.save(
+                        new Depoimento(
+                                "",
+                                "b".repeat(500), "Minha Imagem")))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @DisplayName("Não é possível salvar depoimento com depoimento vazio")
+    @Test
+    public void testCenario9() {
+        // Arrange / Act / Assert
+        assertThatThrownBy(
+                () -> repository.save(
+                        new Depoimento(
+                                "a".repeat(120),
+                                "", "Minha Imagem")))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @DisplayName("Não é possível salvar url de foto com mais de 255 caracteres")
+    @Test
+    public void testCenario10() {
+        // Arrange / Act / Assert
+        assertThatThrownBy(
+                () -> repository.save(
+                        new Depoimento(
+                                "a".repeat(120),
+                                "b".repeat(500), "c".repeat(256))))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @DisplayName("Não é possível salvar url de foto vazia")
+    @Test
+    public void testCenario11() {
+        // Arrange / Act / Assert
+        assertThatThrownBy(
+                () -> repository.save(
+                        new Depoimento(
+                                "a".repeat(120),
+                                "b".repeat(500), "")))
+                .isInstanceOf(ConstraintViolationException.class);
     }
 
 }
