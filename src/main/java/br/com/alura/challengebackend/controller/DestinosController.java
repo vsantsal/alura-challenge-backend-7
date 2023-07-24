0b22c981a60c7f65eaff1da0e7121c5e32070488
vsantsal <vsantsal@gmail.com>
2023-07-24 14:59:25 -0300

    tests: criacao de teste de post para o controller de destino nao compilando ainda

---
+++ src/main/java/br/com/alura/challengebackend/controller/DestinosController.java
@@ -0,0 +1,4 @@
+package br.com.alura.challengebackend.controller;
+
+public class DestinosController {
+}
---
+++ src/test/java/br/com/alura/challengebackend/controller/DestinosControllerTest.java
@@ -0,0 +1,70 @@
+package br.com.alura.challengebackend.controller;
+
+import br.com.alura.challengebackend.domain.entity.Depoimento;
+import br.com.alura.challengebackend.domain.repository.DepoimentosRepository;
+import br.com.alura.challengebackend.service.DepoimentosService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+class DestinosControllerTest {
+
+    private final String ENDPOINT = "/destinos";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SpyBean
+    private DestinosService service;
+
+    @MockBean
+    private DestinosRepository repository;
+
+    @DisplayName("Deve cadastrar com sucesso se dados informados validos")
+    @Test
+    public void testCenario1() throws Exception {
+        // Arrange
+        when(repository.save(any(Destino.class))).thenReturn(
+                new Destino(
+                        "Destino",
+                        new BigDecimal("1234.56"),
+                        "https://www.imagemdestino.com"
+                )
+        );
+
+        // Act
+        this.mockMvc.perform(
+                        post( ENDPOINT)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(
+                                        "{\"nome\": \"Destino\", " +
+                                                "\"preco\": 1234.56," +
+                                                " \"url_foto\": \"https://www.imagemdestino.com\"}" )
+                )
+                // Assert
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", containsString(ENDPOINT)));
+    }
+
+}
